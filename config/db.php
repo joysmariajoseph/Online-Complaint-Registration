38eceb67d1dc358cda99d3ac411dc2400b522e4b
JOYS MARIA JOSEPH <joysmariajoseph@gmail.com>
2016-06-01 15:34:13 +0530

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=hackathon',
+    'username' => 'root',
+    'password' => 'joys',
+    'charset' => 'utf8',
+];
